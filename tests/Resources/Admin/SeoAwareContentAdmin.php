25d200e94b389020ce894fd6674381020d56dc35
ElectricMaxxx <maximilian.berghoff@gmx.de>
2016-12-05 00:30:44 +0100

    add admin web tests for the extensions

--- tests/Resources/Admin/SeoAwareContentAdmin.php
+++ tests/Resources/Admin/SeoAwareContentAdmin.php
@@ -11,45 +11,8 @@
 
 namespace Symfony\Cmf\Bundle\SonataAdminIntegrationBundle\Tests\Resources\Admin;
 
-use Sonata\AdminBundle\Datagrid\DatagridMapper;
-use Sonata\AdminBundle\Datagrid\ListMapper;
-use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\DoctrinePHPCRAdminBundle\Admin\Admin;
-
-class SeoAwareContentAdmin extends Admin
+class SeoAwareContentAdmin extends BaseAdmin
 {
     protected $baseRouteName = 'cmf_seo_aware_content';
     protected $baseRoutePattern = 'cmf/seo/seoawarecontent';
-
-    public function getExportFormats()
-    {
-        return array();
-    }
-
-    protected function configureListFields(ListMapper $listMapper)
-    {
-        $listMapper
-            ->addIdentifier('id', 'text')
-            ->addIdentifier('title', 'text')
-        ;
-    }
-
-    protected function configureFormFields(FormMapper $formMapper)
-    {
-        $formMapper
-            ->with('form.group_general')
-                ->add('name', 'text')
-                ->add('title', 'text')
-                ->add('body', 'textarea', array('required' => false))
-            ->end()
-        ;
-    }
-
-    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
-    {
-        $datagridMapper
-            ->add('title', 'doctrine_phpcr_string')
-            ->add('name', 'doctrine_phpcr_nodename')
-        ;
-    }
 }
