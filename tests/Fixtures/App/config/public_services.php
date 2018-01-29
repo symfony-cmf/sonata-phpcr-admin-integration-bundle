d98933a3f89a492886e8ea3e19d6ff1c6d4595d3
David Buchmann <david@liip.ch>
2018-01-29 08:41:58 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Fixtures/App/config/public_services.php
+++ tests/Fixtures/App/config/public_services.php
@@ -1,6 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony CMF package.
+ *
+ * (c) 2011-2017 Symfony CMF
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 return [
-    'cmf.block.service'
+    'cmf.block.service',
 ];
